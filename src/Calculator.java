1ea42548aab21a64244d4d62e42366407d874133
bizkvit220 <122497550+bizkvit220@users.noreply.github.com>
2023-08-23 12:17:35 +0300

    1

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,19 +12,19 @@ public class Calculator {
         String operatorStr = "";
         char[] symbol = new char[10];
         char operator = '+';
-        for (int i=1; i < inputStr.length(); i++) {
+        for (int i1=1; i1 < inputStr.length(); i1++) {
 
-            symbol[i] = inputStr.charAt(i);
-            if (symbol[i] == '+'){
+            symbol[i1] = inputStr.charAt(i1);
+            if (symbol[i1] == '+'){
                 operator = '+'; operatorStr = "\\+";
             }
-            if (symbol[i] == '-'){
+            if (symbol[i1] == '-'){
                 operator = '-'; operatorStr = "-";
             }
-            if (symbol[i] == '*'){
+            if (symbol[i1] == '*'){
                 operator = '*'; operatorStr = "\\*";
             }
-            if (symbol[i] == '/'){
+            if (symbol[i1] == '/'){
                 operator = '/'; operatorStr = "/";
             }
         }
